94938fb2ac99688cf72c29c8b29d84a55e9b0112
linkalls <naonyan1135@gmail.com>
2025-03-14 00:20:13 +0900

    Next.jsの設定を更新し、画像ドメインの設定をremotePatterns形式に変更

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["avatars.githubusercontent.com"],
-    // または、より細かく設定する場合は以下を使用
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'avatars.githubusercontent.com',
-    //     port: '',
-    //     pathname: '/**',
-    //   },
-    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
