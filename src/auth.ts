7cf7b89780ebbe85e8931708bf41fa4b794495f0
linkalls <naonyan1135@gmail.com>
2025-03-13 21:00:54 +0900

    Add trustHost option to NextAuth configuration

--- src/auth.ts
+++ src/auth.ts
@@ -6,4 +6,5 @@ import GitHub from "next-auth/providers/github";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   adapter: DrizzleAdapter(db),
+  trustHost: true, // ← ここを追加！！
 });
